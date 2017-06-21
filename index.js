62612759870fd928132061dcab2332aec5aecc49
Ben McClure <ben.mcclure@gmail.com>
2017-06-21 14:21:31 -0500

    Convert from function to object export

--- index.js
+++ index.js
@@ -3,43 +3,40 @@ var exec    = require("child_process").exec;
 /*****************************************************************************\
     Enable or disable services with systemctl
 \*****************************************************************************/
-module.exports = function() {
+module.exports = {
+    enable:        _enable,
+    disable:       _disable,
+    start:         _start,
+    stop:          _stop,
+    restart:       _restart,
+    daemon_reload: _daemon_reload,
+    exec:          _exec
+}
 
-    _enable = function (service_name, callback) {
-        _exec("enable " + service_name, callback);
-    };
+function _enable(service_name, callback) {
+    _exec("enable " + service_name, callback);
+};
 
-    _disable = function (service_name, callback) {
-        _exec("disable " + service_name, callback);
-    };
+function _disable(service_name, callback) {
+    _exec("disable " + service_name, callback);
+};
 
-    _start = function (service_name, callback) {
-        _exec("start " + service_name, callback);
-    };
+function _start(service_name, callback) {
+     _exec("start " + service_name, callback);
+};
 
-    _stop = function (service_name, callback) {
-        _exec("stop " + service_name, callback);
-    };
+function _stop(service_name, callback) {
+    _exec("stop " + service_name, callback);
+};
 
-    _restart = function (service_name, callback) {
-        _exec("restart " + service_name, callback);
-    };
+function _restart(service_name, callback) {
+    _exec("restart " + service_name, callback);
+};
 
-    _daemon_reload = function (callback) {
-        _exec("daemon-reload", callback);
-    }
+function _daemon_reload(callback) {
+    _exec("daemon-reload", callback);
+}
 
-    _exec = function (command, callback) {
-        exec("sudo systemctl " + command, callback);
-    }
-
-    return {
-        enable:        _enable,
-        disable:       _disable,
-        start:         _start,
-        stop:          _stop,
-        restart:       _restart,
-        daemon_reload: _daemon_reload,
-        exec:          _exec
-    };
+function _exec(command, callback) {
+    exec("sudo systemctl " + command, callback);
 }
