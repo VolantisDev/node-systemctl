5aa7d60d420353d8ca51738b66040be59d3eb4b5
Ben McClure <ben.mcclure@gmail.com>
2017-06-24 22:41:52 -0500

    Add is_enabled

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-var exec    = require("child_process").exec;
+var exec    = require("child_process").exec
 
 /*****************************************************************************\
     Enable or disable services with systemctl
 \*****************************************************************************/
 module.exports = {
-    enable:        _enable,
-    disable:       _disable,
-    start:         _start,
-    stop:          _stop,
-    restart:       _restart,
+    is_enabled: _is_enabled,
+    enable: _enable,
+    disable: _disable,
+    start: _start,
+    stop: _stop,
+    restart: _restart,
     daemon_reload: _daemon_reload,
-    exec:          _exec
+    exec: _exec
 }
 
 function _enable(service_name, callback) {
     _exec("enable " + service_name, callback);
-};
+}
 
 function _disable(service_name, callback) {
     _exec("disable " + service_name, callback);
-};
+}
 
 function _start(service_name, callback) {
      _exec("start " + service_name, callback);
-};
+}
 
 function _stop(service_name, callback) {
     _exec("stop " + service_name, callback);
-};
+}
 
 function _restart(service_name, callback) {
     _exec("restart " + service_name, callback);
-};
+}
 
 function _daemon_reload(callback) {
     _exec("daemon-reload", callback);
+}
+
+function _is_enabled(service_name, callback) {
+    _exec('is-enabled ' + service_name, (error, stdout, stderr) => {
+        callback(error, (stdout.indexOf('enabled') != -1))
+    })
 }
 
 function _exec(command, callback) {
