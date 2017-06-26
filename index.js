5c9a90bd4bb8ba75dc8e5a6732766f398ff3a9eb
Ben McClure <ben.mcclure@gmail.com>
2017-06-26 15:01:46 -0500

    Convert library to promises

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var exec    = require("child_process").exec
+var Promise = require('bluebird')
+var exec = require("child-process-promise").exec
 
 /*****************************************************************************\
     Enable or disable services with systemctl
@@ -14,36 +15,40 @@ module.exports = {
     exec: _exec
 }
 
-function _enable(service_name, callback) {
-    _exec("enable " + service_name, callback);
+function _enable(service_name) {
+    return _exec("enable " + service_name);
 }
 
-function _disable(service_name, callback) {
-    _exec("disable " + service_name, callback);
+function _disable(service_name) {
+    return _exec("disable " + service_name);
 }
 
-function _start(service_name, callback) {
-     _exec("start " + service_name, callback);
+function _start(service_name) {
+    return _exec("start " + service_name);
 }
 
-function _stop(service_name, callback) {
-    _exec("stop " + service_name, callback);
+function _stop(service_name) {
+    return _exec("stop " + service_name);
 }
 
-function _restart(service_name, callback) {
-    _exec("restart " + service_name, callback);
+function _restart(service_name) {
+    return _exec("restart " + service_name);
 }
 
-function _daemon_reload(callback) {
-    _exec("daemon-reload", callback);
+function _daemon_reload() {
+    return _exec("daemon-reload");
 }
 
-function _is_enabled(service_name, callback) {
-    _exec('is-enabled ' + service_name, (error, stdout, stderr) => {
-        callback(error, (stdout.indexOf('enabled') != -1))
+function _is_enabled(service_name) {
+    return new Promise((resolve, reject) => {
+        _exec('is-enabled ' + service_name)
+            .then((result) => {
+                resolve(result.stdout.indexOf('enabled') != -1)
+            })
+            .catch(reject)
     })
 }
 
-function _exec(command, callback) {
-    exec("sudo systemctl " + command, callback);
+function _exec(command) {
+    return exec("sudo systemctl " + command);
 }
