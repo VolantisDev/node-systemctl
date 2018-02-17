56bba8b36fca058386cc9e9b037bc527cc279ac4
Frank Lemanschik <frank@github.dspeed.eu>
2018-02-17 12:12:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 var exec = require("child-process-promise").exec
 
-/*****************************************************************************\
-    Enable or disable services with systemctl
-\*****************************************************************************/
-module.exports = {
-    isEnabled: isEnabled,
-    enable: enable,
-    disable: disable,
-    start: start,
-    stop: stop,
-    restart: restart,
-    daemon_reload: daemonReload,
-    exec: _exec
-}
-
 // enable, disable, link, start, stop ...
-function run(cmd,service_name) {
+module.exports = function run(cmd,service_name) {
     return exec(`systemctl ${cmd} ${service_name}`);
-}
-
-function isEnabled(service_name) {
-    return _exec('is-enabled ' + service_name)
-        .then((result) => {
-            return (result.stdout.indexOf('enabled') != -1)
-         })
 }
