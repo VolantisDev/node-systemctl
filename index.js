e3884a437499a62957cdf5120a6c1a59d388248b
Frank Lemanschik <frank@github.dspeed.eu>
2018-02-17 12:05:42 +0000

    Clean Up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var Promise = require('bluebird')
 var exec = require("child-process-promise").exec
 
 /*****************************************************************************\
@@ -15,40 +14,14 @@ module.exports = {
     exec: _exec
 }
 
-function enable(service_name) {
-    return _exec("enable " + service_name);
-}
-
-function disable(service_name) {
-    return _exec("disable " + service_name);
-}
-
-function start(service_name) {
-    return _exec("start " + service_name);
-}
-
-function stop(service_name) {
-    return _exec("stop " + service_name);
-}
-
-function restart(service_name) {
-    return _exec("restart " + service_name);
-}
-
-function daemonReload() {
-    return _exec("daemon-reload");
+// enable, disable, link, start, stop ...
+function run(cmd,service_name) {
+    return exec(`systemctl ${cmd} ${service_name}`);
 }
 
 function isEnabled(service_name) {
-    return new Promise((resolve, reject) => {
-        _exec('is-enabled ' + service_name)
-            .then((result) => {
-                resolve(result.stdout.indexOf('enabled') != -1)
-            })
-            .catch(reject)
-    })
-}
-
-function _exec(command) {
-    return exec("sudo systemctl " + command);
+    return _exec('is-enabled ' + service_name)
+        .then((result) => {
+            return (result.stdout.indexOf('enabled') != -1)
+         })
 }
