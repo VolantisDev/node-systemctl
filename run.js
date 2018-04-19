2cfda6aac7bed5129606078a71491870a2f8a384
Ben McClure <ben.mcclure@gmail.com>
2018-04-18 20:34:15 -0500

    Refactor run command slightly and bump version to 0.2.1

--- run.js
+++ run.js
@@ -1,5 +1,11 @@
 var exec = require("child-process-promise").exec
 
 module.exports = function run(cmd, service_name) {
-    return exec(`systemctl ${cmd} ${service_name}`)
+    var command = 'systemctl ' + cmd
+
+    if (service_name) {
+        command = command + ' ' + service_name
+    }
+
+    return exec(command)
 }
